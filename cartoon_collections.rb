4ad9c613097298a1166ad512a9137790c2ee848f
Bryan Rhodunda <rhodunda@udel.edu>
2019-11-17 03:08:32 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,11 +1,11 @@
-def roll_call_dwarves
+def roll_call_dwarves(dwarves)
   dwarves.each_with_index do |name, index|
     index += 1 
     puts "#{index}. #{name}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet
+  
 end
 
 def long_planeteer_calls# code an argument here
